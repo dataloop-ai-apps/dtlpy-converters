03f7c32d4c69894b77993e2d2e2fc7e6b4e05eb4
Shadi Mahameed <shadi.m@dataloop.ai>
2023-08-29 19:19:53 +0300

    updating setup script

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
     requirements = f.read()
 
 packages = [
-    package for package in find_packages() if package == 'dtlpy_converters']
+    package for package in find_packages() if package.startswith('dtlpy_converters')]
 
 setup(name='dtlpy_converters',
       classifiers=[
